7c4e07a18fbb87897a566ce50f1551656fd9115a
Shanaia Fernandes <shanaia0213@gmail.com>
2024-04-29 12:38:12 +0530

    first commit

---
+++ exam/src/examq1/Cat.java
@@ -0,0 +1,19 @@
+package examq1;
+
+public class Cat {
+	
+	private String name;
+	private String colour;
+	public Cat(String name, String breed)
+	{
+		this.name=name;
+		this.colour=colour;
+	}
+	
+	public String getColour()
+	{
+		return colour;
+	}
+	
+
+}
